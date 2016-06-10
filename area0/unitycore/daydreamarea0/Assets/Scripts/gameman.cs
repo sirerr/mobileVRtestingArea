dbaba9c685f65d240ceeeea47fd1dce0af1b4209
sirerr <sirerr@gmail.com>
2016-06-10 01:58:09 -0400

    playing around

---
+++ area0/unitycore/daydreamarea0/Assets/Scripts/gameman.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Gvr.Internal;
+
+public class gameman : MonoBehaviour {
+
+	public static gameman gamemanref;
+
+	public GameObject maincontroller;
+	public GameObject mainplayer;
+
+
+	void Awake()
+	{
+		gamemanref =this;
+		maincontroller = GameObject.FindGameObjectWithTag("controller");
+		mainplayer = GameObject.FindGameObjectWithTag("Player");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		maincontroller.transform.rotation = GvrController.Orientation;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
